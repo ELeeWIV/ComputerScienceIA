f385c21d2340b0a60a944bcc0f8d9752873f0fbf
ELee <elee.wingfield@gmail.com>
2020-01-21 14:34:17 +0000

    encapsulation of GUI1 elements - 2
    create ogs and moved all relevant items to ogs
    created GUI1 package
    changed the ogs and nc classes to types panel
    see code for other main changes

--- GUI.java
+++ GUI.java
@@ -1,148 +1,27 @@
 package Main;
 
-import java.awt.Label;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.WindowListener;
-import java.awt.Button;
-import java.awt.Choice;
-import java.awt.FlowLayout;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
-
-import java.awt.TextArea;
+import java.awt.*;
+import java.swing.*;
 
 public class GUI extends JFrame implements ActionListener, ItemListener{
-	private Label noStudents, noSets, addEquipList, equiplistLabel, teacher, room, date, period;
-	private TextField noStudentsField, noSetsField, addEquipListField, teacherField, roomField, dateField, periodField;
-	private Button addChem, submit;
-	private NewChem aNewChem;
-	private Choice equipList;
-	private String equipmentItem;
-	private TextArea txtArea;
-	private Label chemName, dil, vol;
-	private TextField chemNameField, dilField, volField;
-	private FileCreater flctr;
+
+	private NewChem nc;
+	private OtherGuiStuff ogs;
 
 	public GUI() {
-		noStudents = new Label("Number of Pupils:");
-		noStudentsField =  new TextField(15);
-
-		noSets = new Label("Number of Sets needed:");
-		noSetsField =  new TextField(15);
-
-		addEquipList = new Label("Additional Equipment needed");
-		addEquipListField =  new TextField(15);
-
-		addChem = new Button("New Chemical");
-		addChem.addActionListener((ActionListener) this);
-		submit =  new Button("Submit");
-
-		equipList = new Choice();
-		equipList.addItemListener(this);
-
-		equipList.add("Beaker");
-		equipList.add("Test Tube");
-		equipList.add("Bunsen Burners");
-		equipList.add("Pipettes");
-		equipList.add("Graduated Cylinder");
-		equipList.add("blank");
-
-		equiplistLabel = new Label("Click an item the number of times you want that item");
-		txtArea = new TextArea(20, 30);
-
-
-		setSize(700, 1000);
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setResizable(false);
-		chemName = new Label ("Chemical:");
-		chemNameField = new TextField();
-
-		dil = new Label ("Dilution:");
-		dilField = new TextField();
-
-		vol = new Label("Volume:");
-		volField = new TextField();
-
-		teacher = new Label ("Teacher:");
-		teacherField = new TextField(15);
-
-		room = new Label ("Room:");
-		roomField = new TextField(15);
-
-		date = new Label ("Date:");
-		dateField  = new TextField(15);
-
-		period = new Label("Period:");
-		periodField = new TextField(15);
-
-		setup();
-	}
-
-	public void setup() {
-		this.setLayout(new FlowLayout());
-
-		add(date);
-		add(dateField);
-
-		add(teacher);
-		add(teacherField);
-
-		add(period);
-		add(periodField);
-
-		add(room);
-		add(roomField);
-
-		add(noStudents);
-		add(noStudentsField);
-
-		add(noSets);
-		add(noSetsField);
-
-		add(equipList);
-		add(equiplistLabel);
-		add(txtArea);
-
-		add(addEquipList);
-		add(addEquipListField);
-
-		add(addChem);
-
-		setVisible(true);
-	}
-
-	public String getDate() {
-		return dateField.getText();
-	}
-
-	public String getTeacher() {
-		return teacherField.getText();
-	}
-
-	public String getroom() {
-		return roomField.getText();
-	}
-
-	public String getNoStudents() {
-		return noStudentsField.getText();
-	}
-
-	public String getNoSets() {
-		return noSetsField.getText();
-	}
-
-	public String getEquip() {
-		return addEquipListField.getText();
-	}
-
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		equipmentItem = equipList.getSelectedItem();
-		txtArea.append(equipmentItem + '\n');
+		Swingutilities.invokeLater(new Runnable() {
+			public void run() {
+				super("Chemistry Department Organizer");
+				setLayout(new BoarderLayout());
+				nc = new NewChem();
+				add(nc, setBoarderLayout.LEFT);
+				ogs = new OtherGuiStuff();
+				add(ogs, setBoarderLayout.RIGHT);
+				this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				this.setSize(600, 500);
+				this.setVisible(true);
+			}
+		})
 
 	}
 
--- NewChem.java
+++ NewChem.java
@@ -1,30 +1,30 @@
-package Main;
+package GUI1;
 import java.awt.Label;
 import java.awt.TextField;
 
-public class NewChem extends GUI {
+public class NewChem extends JPanel {
 	private Label chemName, dil, vol;
 	private TextField chemNameField, dilField, volField;
-	
+
 	public NewChem() {
 		chemName = new Label ("Chemical:");
 		chemNameField = new TextField();
-		
+
 		dil = new Label ("Dilution:");
 		dilField = new TextField();
-		
+
 		vol = new Label("Volume:");
 		volField = new TextField();
-		
+
 	}
-	
+
 	public void addChemical() {
 		add(chemName);
 		add(chemNameField);
-		
+
 		add(dil);
 		add(dilField);
-		
+
 		add(vol);
 		add(volField);
 	}
